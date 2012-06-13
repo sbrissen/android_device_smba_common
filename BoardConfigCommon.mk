08cacced4642555a10cc6e9b7386a4b7571012a2
wjb <jaybob413@gmail.com>
2012-06-13 19:52:30 -0400

    update board config and device makefiles for device/common split

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,77 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+USE_CAMERA_STUB := false
+
+TARGET_PREBUILT_KERNEL := device/malata/smba-common/prebuilt/kernel
+
+TARGET_SPECIFIC_HEADER_PATH := device/malata/smba-common/include
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+PRODUCT_CHARACTERISTICS := tablet
+
+#platform
+TARGET_BOARD_PLATFORM := tegra
+TARGET_BOOTLOADER_BOARD_NAME := harmony
+
+#arhitecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+#audio
+BOARD_USES_GENERIC_AUDIO := true
+BOARD_USES_AUDIO_LEGACY := false
+
+#bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+#graphics
+BOARD_EGL_CFG := device/malata/smba-common/prebuilt/egl.cfg
+BOARD_USES_OVERLAY := true
+USE_OPENGL_RENDERER := true
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+ 
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_PAGE_SIZE := 0x00000800
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0FA00000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_USES_SECURE_SERVICES := true
+
+TARGET_OTA_ASSERT_SKIP := true
---
+++ device-common.mk
@@ -0,0 +1,98 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS += device/malata/smba-common/overlay
+
+$(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
+
+# uses mdpi artwork where available
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+PRODUCT_CHARACTERISTICS := tablet
+
+# HAL
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.primary.tegra \
+    libaudioutils \
+    sensors.tegra \
+    lights.tegra \
+    gps.tegra \
+    libmbm-ril
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	setup_fs
+
+PRODUCT_PACKAGES += \
+	librs_jni \
+	com.android.future.usb.accessory 
+
+# These are the hardware-specific feature permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml 
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Media
+PRODUCT_COPY_FILES += \
+    device/malata/smba-common/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+
+# Harmony Configs
+PRODUCT_COPY_FILES += \
+    device/malata/smba-common/prebuilt/init.harmony.rc:root/init.harmony.rc \
+    device/malata/smba-common/prebuilt/init.harmony.usb.rc:root/init.harmony.usb.rc \
+    device/malata/smba-common/prebuilt/ueventd.harmony.rc:root/ueventd.harmony.rc \
+    device/malata/smba-common/prebuilt/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    device/malata/smba-common/prebuilt/nvram.txt:system/etc/wifi/nvram.txt \
+    device/malata/smba-common/prebuilt/sysctl.conf:system/etc/sysctl.conf
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf \
+    device/malata/smba-common/prebuilt/bcm4329.hcd:system/etc/firmware/bcm4329.hcd
+
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    device/malata/smba-common/prebuilt/at168_touch.idc:system/usr/idc/at168_touch.idc 
+
+# Generic
+PRODUCT_COPY_FILES += \
+   device/malata/smba-common/prebuilt/vold.fstab:system/etc/vold.fstab
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    BUILD_UTC_DATE=0 \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15
+
+WIFI_BAND := 802_11_ABG
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
