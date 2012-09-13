b2d9e83147a72cf7831859a8167d3d7f6ed988e8
Zak Peirce <plastikman@gmail.com>
2012-09-13 10:39:58 -0700

    new compcache and frameworks for new blobs

--- device-common.mk
+++ device-common.mk
@@ -76,6 +76,7 @@ PRODUCT_COPY_FILES += \
     device/malata/smba_common/prebuilt/init.harmony.usb.rc:root/init.harmony.usb.rc \
     device/malata/smba_common/prebuilt/ueventd.harmony.rc:root/ueventd.harmony.rc \
     device/malata/smba_common/prebuilt/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    device/malata/smba_common/prebuilt/compcache:system/bin/compcache \
     device/malata/smba_common/prebuilt/02zram:system/etc/init.d/02zram \
     device/malata/smba_common/prebuilt/03sysctl:system/etc/init.d/03sysctl \
     device/malata/smba_common/prebuilt/nvram.txt:system/etc/wifi/nvram.txt 
