d2681b14332abab868e6fde7e0f8b4f1baae1f2f
fosser2 <fosser2@gmail.com>
2013-02-07 14:43:43 -0600

    smba: start of device tree restructure

--- device-common.mk
+++ device-common.mk
@@ -14,21 +14,35 @@
 # limitations under the License.
 #
 
-DEVICE_PACKAGE_OVERLAYS += device/malata/smba_common/overlay
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/cm/prebuilt/common/bootanimation/600.zip:system/media/bootanimation.zip
+endif
 
-$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+# Harmony Configs
+PRODUCT_COPY_FILES += \
+    device/malata/smba_common/prebuilt/ramdisk/init.rc:root/init.rc \
+    device/malata/smba_common/prebuilt/ramdisk/init.harmony.usb.rc:root/init.harmony.usb.rc \
+    device/malata/smba_common/prebuilt/ramdisk/ueventd.harmony.rc:root/ueventd.harmony.rc \
+    device/malata/smba_common/prebuilt/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+	device/malata/smba_common/prebuilt/compcache:system/bin/compcache \
+    device/malata/smba_common/prebuilt/nvram.txt:system/etc/wifi/nvram.txt
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    BUILD_UTC_DATE=0 \
+    wifi.interface=wlan0 \
+	ro.zram.default=18 \
+    wifi.supplicant_scan_interval=180
 
 # uses mdpi artwork where available
-PRODUCT_AAPT_CONFIG := normal mdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
-PRODUCT_LOCALES += mdpi
+	PRODUCT_AAPT_CONFIG := normal mdpi
+	PRODUCT_AAPT_PREF_CONFIG := mdpi
+	PRODUCT_LOCALES += mdpi
 
 LOCAL_ACP_UNAVAILABLE := false
 
-PRODUCT_CHARACTERISTICS := tablet
-
 # HAL
-#  temp remove libmbm-ril
+
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.harmony \
@@ -38,73 +52,70 @@ PRODUCT_PACKAGES += \
     lights.tegra \
     camera.tegra \
     sensors.tegra
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-        librs_jni \
-	setup_fs \
-        make_ext4fs \
-        l2ping \
-        hcitool \
-        bttest
-
-# These are the hardware-specific feature permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml 
-
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-PRODUCT_PACKAGES += \
-  com.android.future.usb.accessory \
-  libnetcmdiface 
-  
-
+	
 # Media
 PRODUCT_COPY_FILES += \
     device/malata/smba_common/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
     device/malata/smba_common/prebuilt/media_codecs.xml:system/etc/media_codecs.xml \
     device/malata/smba_common/prebuilt/audio_policy.conf:system/etc/audio_policy.conf
-
-# Harmony Configs
+	
+# These are the hardware-specific feature permissions this was commented out
 PRODUCT_COPY_FILES += \
-    device/malata/smba_common/prebuilt/ramdisk/init.rc:root/init.rc \
-    device/malata/smba_common/prebuilt/ramdisk/init.harmony.usb.rc:root/init.harmony.usb.rc \
-    device/malata/smba_common/prebuilt/ramdisk/ueventd.harmony.rc:root/ueventd.harmony.rc \
-    device/malata/smba_common/prebuilt/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/malata/smba_common/prebuilt/nvram.txt:system/etc/wifi/nvram.txt
-
-PRODUCT_COPY_FILES += \
-   device/malata/smba_common/prebuilt/make_ext4fs:system/bin/make_ext4fs
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml 
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf \
-    device/malata/smba_common/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf 
-   
+	system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf \
+	device/malata/smba_common/prebuilt/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+	device/malata/smba_common/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+	
 # Touchscreen
 PRODUCT_COPY_FILES += \
     device/malata/smba_common/prebuilt/at168_touch.idc:system/usr/idc/at168_touch.idc
+	
+PRODUCT_CHARACTERISTICS := tablet
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    BUILD_UTC_DATE=0 \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=180
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory \
+	libnetcmdiface 
+  
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	librs_jni \
+	setup_fs \
+	make_ext4fs \
+	l2ping \
+	hcitool \
+	bttest
+
+DEVICE_PACKAGE_OVERLAYS += device/malata/smba_common/overlay
+
+# for bugmailer
+ifneq ($(TARGET_BUILD_VARIANT),user)
+	PRODUCT_PACKAGES += send_bug
+	PRODUCT_COPY_FILES += \
+		system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
+		system/extras/bugmailer/send_bug:system/bin/send_bug
+endif
 
 WIFI_BAND := 802_11_ABG
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
 
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
